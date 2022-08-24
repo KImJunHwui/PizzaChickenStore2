cd284b44e1741267d3806e873c60a0491d8fe6e9
KJH <daeding7@naver.com>
2022-08-24 12:44:43 +0900

    Initial commit

---
+++ app/src/main/java/com/kim/pizzachickenstore2/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.kim.pizzachickenstore2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
